0ccbdb95d08575052d6a4e917c23a42ed423cd08
c-mar-dev <charlesmartinet2000@gmail.com>
2025-12-23 00:42:51 +0100

    Fix undefined fields in cards by making props reactive

--- src/lib/components/DecisionCard.svelte
+++ src/lib/components/DecisionCard.svelte
@@ -29,9 +29,9 @@
 
   $: selectedTaskCount = Object.values(selectedTasks).filter(Boolean).length;
 
-  const config = decisionTypeConfig[decision.decisionType];
-  const thingConfig = thingTypeConfig[decision.subject.type];
-  const data = decision.data || {};
+  $: config = decisionTypeConfig[decision.decisionType];
+  $: thingConfig = thingTypeConfig[decision.subject.type];
+  $: data = decision.data || {};
 </script>
 
 <div class="max-w-4xl mx-auto p-8">
